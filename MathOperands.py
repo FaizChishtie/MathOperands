ff94826cc19f8051439801ac5d60c77b3f65882f
FaizChishtie <31967593+FaizChishtie@users.noreply.github.com>
2017-09-15 11:46:42 -0400

    Add files via upload

---
+++ MathOperands.py
@@ -0,0 +1,23 @@
+import math
+
+class vector_operands:
+
+    """ |v||u|cos(a) = a1b1 + a2b2 ..."""
+    def __init__(self, vector_1, vector_2):
+        self.v1 = vector_1
+        self.v2 = vector_2
+        return
+
+    def dot_product(self):
+        self.sum_comp()
+
+        return
+
+    def sum_comp(self):
+        sum = 0
+        for i in v1:
+            for j in v2:
+                sum += i*j
+        return
+
+    def magnitude(self):
